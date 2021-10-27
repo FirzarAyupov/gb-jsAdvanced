14464cd504182bea4ce4776be1cfaea4062965a3
FirzarAyupov <66380680+FirzarAyupov@users.noreply.github.com>
2021-10-27 19:42:44 +0300

    hw4

--- js/anim.js
+++
@@ -1,8 +0,0 @@
-let cartBtn = document.querySelector('.btn-cart');
-let cart = document.querySelector('.cart');
-
-let cartList = document.createElement('div');
-
-cartBtn.addEventListener('click', function () {
-    cart.classList.toggle('cart-hide');
-});
